26b06940199ea20cc5ace3f9eb70606b9f3b5760
sameer266 <sameerbaiju792@gmail.com>
2024-10-05 02:24:09 +0545

    Your commit message here

--- mynews/settings.py
+++ mynews/settings.py
@@ -131,12 +131,13 @@ USE_TZ = True
 # Static files
 import os
 # URL prefix for serving static files
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles" / "static"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Media files settings
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
