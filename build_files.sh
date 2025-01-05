5a5c1aa4caddbed98865f9858373da74c94e042d
sameer266 <sameerbaiju792@gmail.com>
2025-01-05 20:57:21 +0545

    update

--- build_files.sh
+++ build_files.sh
@@ -1,19 +1,26 @@
-echo " BUILD START"
+echo "BUILD START"
 
-
+# Check Python version
 python3.9 --version
+
+# Upgrade pip and ensure it’s installed
 python3.9 -m pip install --upgrade pip
 python3.9 -m ensurepip 
-python3.9 -m pip install -r requirements.txt
 
-echo " MAKE MIGRATIONS..."
+# Install dependencies from requirements.txt
+python3.9 -m pip install --no-cache-dir -r requirements.txt
+
+
+echo "MAKE MIGRATIONS..."
+# Make and apply migrations
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
+# Collect static files
 python3.9 manage.py collectstatic --noinput
 
 # List the contents of the staticfiles directory to verify
 echo "Contents of staticfiles directory:"
 ls -l staticfiles
 
-echo " BUILD END"
+echo "BUILD END"
